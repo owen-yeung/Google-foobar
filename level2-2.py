a5639f998e988bd1fe414ed2fc78c91920e69d6c
owen-yeung <owen_yeung@berkeley.edu>
2023-10-05 12:05:13 +0800

    start level 2-2

---
+++ level2-2.py
@@ -0,0 +1,16 @@
+def solution(xs):
+    # Your code here
+
+'''
+-- Python cases --
+Input:
+solution.solution([2, 0, 2, 2, 0])
+Output:
+    8
+
+Input:
+solution.solution([-2, -3, 4, -5])
+Output:
+    60
+
+    '''
