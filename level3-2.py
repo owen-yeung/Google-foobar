5cdcf415b67a2609aabe9ac4d5c78406e82a2814
owen-yeung <owen_yeung@berkeley.edu>
2023-10-09 01:23:54 +0800

    3-2 done

--- level3-2.py
+++ level3-2.py
@@ -1,65 +1,60 @@
 '''
 Challenge: Return number of triples (a,b,c) in a list that satisfy c % b == 0 and b % a == 0
 '''
+'''
+raw: 
+1,2,3,4,5,6
+number of prev items divisible:
+0,1,1,2,1,3
+number of triples:
+0,0,0,1,0,2
 
+raw: 
+1,2,4,8,16,32
+number of prev items divisible:
+0,1,2,3,4,5
+number of triples:
+0,0,1,3,6,10
+
+raw: 
+1,1,1,1,2,6,8,12
+number of prev items divisible:
+0,1,2,3,4,5,5,6
+number of triples:
+0,0,1,3,...
+'''
 # probably not a runtime problem? there exists a "took too long" error
-def solution(l):
-    '''
-    algorithm: work backwards from the list
-    let z = last item
-    extract all ints that divide z
-    count number of lucky triples with z
-    remove z from list and repeat
-    (May have to count and trip 1s for recursion depth)
-    '''
-    if len(l) < 3:
-        return 0
-    count = 0
-    while len(l) >= 3:
-        z = l[-1]
-        l = l[:-1]
-        div_z = []
-        for x in l:
-            if z % x == 0:
-                div_z.append(x)
-        count += num_pairs(div_z)
-    return count
-def num_pairs(lst):
-    '''
-    Take a list of ints that divide z
-    :return: number of pairs s.t. lst[i]|lst[j] and i<j
-    algorithm:
-    - start from last item
-    - loop through list and check for divis
-    - go to the next last item
-    - end at first item
-    '''
 
-    count = 0
-    # catch ones
-    # generalize: if there's a number that divides every number behind,
-    # oneless = []
-    # length = len(lst)
-    # for i in range(length):
-    #     if lst[i] == 1:
-    #         count += length - i - 1
-    #     else:
-    #         oneless.append(lst[i])
-    # lst = oneless
-    length = len(lst)
-    for i in range(length - 1, 0, -1):
+def num_pairs(l):
+    '''
+    :param l: list of ints between 1-999999
+    :return: list x where x[i] = of how many prev numbers of l[i] are divisible
+    '''
+    length = len(l)
+    num_pairs = length * [0]
+    for i in range(length):
         for j in range(i):
-            if lst[i] % lst[j] == 0:
-                count += 1
-    return count
+            if l[i] % l[j] == 0:
+                num_pairs[i] += 1
+    return num_pairs
 
-# print(solution(range(1, 2000)))
-# print([solution([1] * i) for i in range(3, 20)])
-# l = [1,2,3]
-# print(l[:-1])
-print(solution([1, 2, 3, 4, 5, 6]), solution([1,1,1]))
+def num_triples(l, num_pairs):
+    '''
+    If l[i] % l[j] == 0: add num_pairs[j] to num_triples[i]
+    :param l:
+    :return: list x where x[i] is sum of num_pair counts of all divisible prev x[j]
+    '''
+    length = len(l)
+    num_triples = length * [0]
+    for i in range(length):
+        for j in range(i):
+            if l[i] % l[j] == 0:
+                num_triples[i] += num_pairs[j]
+    return num_triples
 
-# Check mutating list
+def solution(l):
+    return sum(
+        num_triples(l, num_pairs(l))
+    )
 
-# if list is sorted, is there a faster algorithm?
-
+print(solution([44] * 1000 + [55] * 1000))
