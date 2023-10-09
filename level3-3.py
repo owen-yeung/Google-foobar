84e0a40563763b5d98b4af0d6a5606421611024f
owen-yeung <owen_yeung@berkeley.edu>
2023-10-09 15:36:14 +0800

    trying dynamic

--- level3-3.py
+++ level3-3.py
@@ -2,89 +2,70 @@
 Write a function called solution(n) that takes a positive integer n and returns the number of different staircases that can be built from exactly n bricks.
 n will always be at least 3 (so you can have a staircase at all), but no more than 200
 '''
+import numpy as np
+class Stair_calculator:
+    max_bricks = 200
+    def __init__(self):
+        self.table = -np.ones((self.max_bricks + 1, self.max_bricks + 1), np.int_)
 
-'''
-Algorithm:
-Step 1: compute the n*h triangular matrix of n_stairs_max_height(n, h)
-'''
+    def write_entry(self, height, bricks, val):
+        '''
+        Adds the entry to the lookup table
+        :param height:
+        :param bricks:
+        :return:
+        '''
+        self.table[height][bricks] = val
 
-def n_stairs_max_height(height, bricks):
-    if bricks < 3:
-        return 0
-    # max height is bricks - 1
-    if height > bricks - 1:
-        return 0
-    if height <= 1:
-        return 0
-    count = 0
-    for h in range(2, height):
-        count += n_stairs_with_height(h)
-    return count
+    def fill_table(self):
+        for bricks in range(3, Stair_calculator.max_bricks + 1):
+            for height in range(2, bricks):
+                self.n_stairs_with_height(height, bricks)
 
-def write_lookup():
-    '''
-    Compute the lookup table
-    :return:
-    '''
-
-def write_to_lookup(height, bricks, table):
-    '''
-    Adds the entry to the lookup table
-    :param height:
-    :param bricks:
-    :return:
-    '''
-    table[bricks][height] = n_stairs_with_height(height, bricks)
-def n_stairs_with_height(height, bricks):
-    '''
-    May have to add lookup for speed (use lookup if value exists, otherwise compute)
-    :param height:
-    :param bricks: ensure height < bricks
-    :return: 0 if no valid stairs
-    '''
-    # base case:
-    if bricks < 3:
-        return 0
-    # max height is bricks - 1
-    if height > bricks - 1:
-        return 0
-    if height <= 1:
-        return 0
-    count = 0
-    bricks_left = bricks - height
-    if height > bricks // 2:
-        for h in range(2, bricks):
-            count += n_stairs_with_height(h, bricks_left)
-        return 1 + count
-    else:
-        for h in range(2, height):
-            count += n_stairs_with_height(h, bricks_left)
+    def n_stairs_with_height(self, height, bricks):
+        if bricks < 3:
+            return 0
+        # max height is bricks - 1
+        if height > bricks - 1:
+            return 0
+        if height <= 1:
+            return 0
+        if self.in_table(height, bricks):
+            return self.get_entry(height, bricks)
+        count = 0
+        bricks_left = bricks - height
+        if height > bricks // 2:
+            count = 1
+            for h in range(2, bricks):
+                count += self.n_stairs_with_height(h, bricks_left)
+        else:
+            for h in range(2, height):
+                count += self.n_stairs_with_height(h, bricks_left)
+        self.write_entry(height, bricks, count)
         return count
-    # we lock in the first layer with height, so bricks left is bricks - height
 
-    # 1 to account for trivial solution (height, bricks - height),
-    # will not be returned by n_stairs_any_height as it is not a substair, only 1 step
+    def in_table(self, height, bricks):
+        return self.table[height][bricks] >= 0
 
+    def get_entry(self, height, bricks):
+        return self.table[height][bricks]
 
-def n_stairs_any_height(bricks):
-    '''
+    def n_stairs_any_height(self, bricks):
+        '''
 
-    :param bricks:
-    :return:
-    '''
-    # base case: when will
-    if bricks < 3:
-        return 0
-    # min height is max h where T(h) <= bricks
-    # min_height =
-    n_stairs = 0
-    for h in range(2, bricks):
-        n_stairs += n_stairs_with_height(h, bricks)
-    return n_stairs
+        :param bricks:
+        :return:
+        '''
+        # base case: when will
+        if bricks < 3:
+            return 0
+        # min height is max h where T(h) <= bricks
+        # min_height =
+        n_stairs = 0
+        for h in range(2, bricks):
+            n_stairs += self.n_stairs_with_height(h, bricks)
+        return n_stairs
 
-# print([(i, n_stairs_any_height(i)) for i in range(5,29)])
-# print(n_stairs_any_height(200))
-table = [[0 for i in range(10)] for i in range(10)]
-print(table[3][2])
-write_to_lookup(2,3,table)
-print(table[3][2])
+def solution(n):
+    s = Stair_calculator()
+    return s.n_stairs_any_height(n)
